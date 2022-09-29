dc95f13fd2dadbc7a73d8ba6bc43082bf34476ab
Faizan Dar <42547319+faizan1234dar@users.noreply.github.com>
2022-09-29 21:11:49 +0530

    updated app.js
    removed the names of students.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,7 @@ function App() {
 
     <>
       <h1>Welcome Interns</h1>
-      <h1>Sunjida Akter</h1>
-       <h1>Abhay Baranwal</h1>
-       	<h2>Hello...I'm Anindya</h2>
-      <h1>Hi I am Mohammad Palla </h1>
-            <h2>im Nazif</h2>
-
-         <h2>It's Aditi here</h2>
-        <h1> I am Ramprasad </h1>
+   <h2>This project is under work</h2>
     
 
 
